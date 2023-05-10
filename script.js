314eb633d6fcf6fd3c449cd11b002adde78c76c3
K Santhosh <87223844+Ksanthosh20ka1a0554@users.noreply.github.com>
2023-05-10 23:36:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var accounts = document.getElementById('accounts');
+function hover(accounts) {
+    element.setAttribute('src', 'http://dummyimage.com/100x100/eb00eb/fff');
+  }
+  
+  function unhover(accounts) {
+    element.setAttribute('src', 'http://dummyimage.com/100x100/000/fff');
+  }
